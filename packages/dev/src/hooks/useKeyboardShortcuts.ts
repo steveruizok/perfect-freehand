426d087cf8c292e1f828143692f45593c6f3af45
Steve Ruiz <steveruizok@gmail.com>
2021-09-17 11:12:43 +0100

    Update useKeyboardShortcuts.ts

--- packages/dev/src/hooks/useKeyboardShortcuts.ts
+++ packages/dev/src/hooks/useKeyboardShortcuts.ts
@@ -11,6 +11,14 @@ export function useKeyboardShortcuts() {
     app.redo()
   })
 
+  useHotkeys('command+c,ctrl+c', () => {
+    app.copySvg()
+  })
+
+  useHotkeys('command+shift+c,ctrl+shift+c', () => {
+    app.copyStyles()
+  })
+
   useHotkeys('e,backspace', () => {
     app.resetDoc()
   })
