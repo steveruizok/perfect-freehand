79e4ef96045ab2aad25d25f2d4d1412b2951af61
Steve Ruiz <steveruizok@gmail.com>
2021-10-05 12:59:48 +0100

    Updates README

--- build.mjs
+++ build.mjs
@@ -4,18 +4,27 @@ import fs from 'fs'
 // and another in the perfect-freehand package folder. When we build
 // the project, we want to replace the older README with the newer.
 
-const pathA = 'README.md'
-const pathB = './packages/perfect-freehand/README.md'
+const files = [
+  'README.md',
+  'process.gif',
+  'icons.png',
+  'perfect-freehand-card.png',
+  'perfect-freehand-logo.svg',
+]
 
-if (
-  new Date(fs.statSync(pathA).mtime).getTime() >
-  new Date(fs.statSync(pathB).mtime).getTime()
-) {
-  // A is newer; remove B and replace with A
-  fs.rmSync(pathB)
-  fs.copyFileSync(pathA, pathB)
-} else {
-  // B is newer; remove A and replace with B
-  fs.rmSync(pathA)
-  fs.copyFileSync(pathB, pathA)
+for (const file of files) {
+  const pathA = file
+  const pathB = `./packages/perfect-freehand/${file}`
+  if (
+    new Date(fs.statSync(pathA).mtime).getTime() >
+    new Date(fs.statSync(pathB).mtime).getTime()
+  ) {
+    // A is newer; remove B and replace with A
+    fs.rmSync(pathB)
+    fs.copyFileSync(pathA, pathB)
+  } else {
+    // B is newer; remove A and replace with B
+    fs.rmSync(pathA)
+    fs.copyFileSync(pathB, pathA)
+  }
 }
