7d19c9412d6bcf9da2b592510cd0d8481bd8367d
Cheng Lou <chenglou92@gmail.com>
2022-09-17 23:31:14 -0700

    Structure-of-array transform

--- packages/dev/src/state/shapes/draw.tsx
+++ packages/dev/src/state/shapes/draw.tsx
@@ -233,23 +233,17 @@ export class DrawUtil extends TLShapeUtil<T, E> {
 
 const average = (a: number, b: number) => (a + b) / 2
 
-function getSvgPathFromStroke(points: number[][]): string {
-  const len = points.length
+function getSvgPathFromStroke(points: {xs: number[], ys: number[]}): string {
+  const len = xs.length
 
   if (!len) {
     return ''
   }
 
-  const first = points[0]
-  let result = `M${first[0].toFixed(3)},${first[1].toFixed(3)}Q`
+  let result = `M${xs[0].toFixed(3)},${ys[0].toFixed(3)}Q`
 
   for (let i = 0, max = len - 1; i < max; i++) {
-    const a = points[i]
-    const b = points[i + 1]
-    result += `${a[0].toFixed(3)},${a[1].toFixed(3)} ${average(
-      a[0],
-      b[0]
-    ).toFixed(3)},${average(a[1], b[1]).toFixed(3)} `
+    result += `${xs[i].toFixed(3)},${ys[i].toFixed(3)} ${average(xs[i], xs[i + 1]).toFixed(3)},${average(ys[i], ys[i + 1]).toFixed(3)} `
   }
 
   result += 'Z'
